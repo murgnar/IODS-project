1ac618ae369a86383b870067849f8d9d6a556307
Gale <gale@ad.helsinki.fi>
2021-11-14 18:06:47 +0200

    Week 2

---
+++ data/create_learning2014.R
@@ -0,0 +1,11 @@
+# Johannes Gale
+# 14.11.
+# Assignement week2
+
+a <- read.delim("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", header = TRUE, sep="\t")
+a
+a
+str(a)
+head(a)
+dim(a)
+dimensions are 183 and 60, very intresting indeed 
